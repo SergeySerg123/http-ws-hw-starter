285f15128f14bad66891274f29885ddd8d0fa623
Sergey <sergey_chizhik@ukr.net>
2021-02-14 11:09:53 +0200

    Create game.js

---
+++ socket/game.js
@@ -0,0 +1,16 @@
+let rooms = [];
+
+export default io => {
+    io.on('connection', socket => {
+        
+        io.on('CREATE_ROOM', () => {
+    
+        });
+    
+        io.on('JOIN_ROOM', () => {
+            
+        });
+    });  
+};
+
+ 
